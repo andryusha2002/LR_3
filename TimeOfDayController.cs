0b0685209a69fcc56e449f075c4f3d030fd3b73b
andryusha2002 <andrey.fedorenko2002@gmail.com>
2024-02-19 22:15:28 +0200

    first

---
+++ TimeOfDayController.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApplication1
+{ 
+using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class TimeOfDayController : ControllerBase
+{
+    private readonly ITimeOfDayService _timeOfDayService;
+
+    public TimeOfDayController(ITimeOfDayService timeOfDayService)
+    {
+        _timeOfDayService = timeOfDayService;
+    }
+
+    [HttpGet]
+    public ActionResult<string> GetTimeOfDay()
+    {
+        var timeOfDay = _timeOfDayService.GetTimeOfDay();
+        return Ok(timeOfDay);
+    }
+}
+
+}
